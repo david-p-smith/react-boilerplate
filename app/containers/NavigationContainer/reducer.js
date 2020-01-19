359cc2e1016f462227446ca30aaed18bffa24ffa
DPS <smith.dp@gmail.com>
2020-01-19 16:43:19 +0000

    initial data for nav

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -4,12 +4,25 @@
  *
  */
 
-import { fromJS } from 'immutable';
-import {
-  DEFAULT_ACTION,
-} from './constants';
+import { fromJS } from "immutable";
+import { DEFAULT_ACTION } from "./constants";
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  topics: [
+    {
+      name: "libraries",
+      description: "links to useful open source libraries"
+    },
+    {
+      name: "apps",
+      description: "links to new and exciting apps"
+    },
+    {
+      name: "news",
+      description: "links to programming related news articles"
+    }
+  ]
+});
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
